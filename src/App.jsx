4588490001f790c73d8db17a3862345f8f1dd986
HarishBhatta <bhattaharish10@gmail.com>
2024-01-19 18:23:35 +0545

    Structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './style';
+import NavBar from './components/NavBar';
+import Billing from './components/Billing';
+import Business from './components/Business';
+import CardDeal from './components/CardDeal';
+import Clients from './components/Clients';
+import CTA from './components/CTA';
+import Stats from './components/stats';
+import Testimonials from './components/Testimonials';
+import View from './components/View';
+import Footer from './components/Footer';
 const App = () => {
   return (
+    <>
       <div className="bg-primary w-full overflow-hidden">
-        <h1>Hello World</h1>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <NavBar />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <View />
+          </div>
+        </div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA /> 
+            <Footer /> 
+          </div>
+        </div>
       </div>
+    </>
+    
     )
 }
 
